9cdc6d22552502aee1ff83b74a8589f3d8919f8a
Steve99-coder <stevenderitu99@gmail.com>
2021-02-01 01:32:40 +0300

    created models ifor post, neighbourhood,Bussiness

--- project/models.py
+++ project/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from users.models import Profile
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+import datetime as dt
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hood')
+    neighbourhood_logo = CloudinaryField('image')
+    description = models.TextField()
+    health_number = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+    occupant_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}hood'
+
+    def save_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+    @classmethod
+    def find_neighbourhood(cls, neighbourhood_id):
+        return cls.objects.filter(id=neighbourhood_id)
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name}Business'
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='hood_post')
+
+    def __str__(self):
+        return f'{self.title} Post'
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
