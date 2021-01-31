0051679b9be6cafa6a19a1544f96c12ee39fcb1c
Steve99-coder <stevenderitu99@gmail.com>
2021-02-01 02:03:28 +0300

    created a user profileupdateform

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,8 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
