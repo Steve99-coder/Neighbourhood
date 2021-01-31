949d5895b421439ecef7aa7871c5de3a9c9c6be2
Steve99-coder <stevenderitu99@gmail.com>
2021-02-01 01:35:30 +0300

    made importation in view file

--- project/views.py
+++ project/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from .forms import BusinessForm, PostForm, NeighbourHoodForm
+from .models import Neighbourhood,Business,Post
+from users.models import Profile
 
 # Create your views here.
