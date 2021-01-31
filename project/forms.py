46f81e8b5042df08250dd4a6aa577be54a47f4b8
Steve99-coder <stevenderitu99@gmail.com>
2021-02-01 01:51:04 +0300

    created a business and postform

--- project/forms.py
+++ project/forms.py
@@ -6,3 +6,14 @@ class NeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
         exclude = ('admin',)
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
